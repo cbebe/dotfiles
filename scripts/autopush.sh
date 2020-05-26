f5fbe2a8d26e866e29cc707dd8dc457cde255da4
Charles Ancheta <cancheta@ualberta.ca>
2020-05-26 00:14:53 -0600

    added comments

--- scripts/autopush.sh
+++ scripts/autopush.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
+
+# get commit message from arguments
 if [ -z "$1" ]; then
     MESSAGE="Autopushed"
 else
     MESSAGE="$1"
 fi
 echo "Pushing to github with message: $MESSAGE"
+# look for .git directory before staging changes
 DOTGITDIR=$(find . -name .git -type d -print)
 while [ -z "$DOTGITDIR" ]; do
     cd ..
     DOTGITDIR=$(find . -name .git -type d -print)
 done
+# now push
 git add .
 git commit -m "$MESSAGE"
 git push
