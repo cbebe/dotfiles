7d0f01eb2770e1c1099d66b9e8b06bc7755557f7
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-06-23 01:04:38 -0600

    fix gen machine script

--- gen_machine_nix.sh
+++ gen_machine_nix.sh
@@ -2,4 +2,4 @@
 
 # https://www.reddit.com/r/NixOS/comments/ae9q01/how_to_os_from_inside_a_nix_file/
 
-echo "{ hostname = \"$(hostname)\"; operatingSystem = \"$(uname)\"; }" | tee -a ~/.config/nixpkgs/machine.nix
+echo "{ hostname = \"$(hostname)\"; operatingSystem = \"$(uname)\"; }" | tee -a $HOME/.dotfiles/config/.config/nixpkgs/machine.nix
