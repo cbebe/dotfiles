53eee827cf336147630b7493e6dfd60431f1494f
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-06-25 12:37:03 -0600

    add darwin config

--- config/.config/nixpkgs/darwin/configuration.nix
+++ config/.config/nixpkgs/darwin/configuration.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages =
