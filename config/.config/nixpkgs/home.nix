6ca8fa05b536203d3a0917af87685a24dad17839
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-06-29 20:59:08 -0600

    Clean up home config
    - Define repeated values
    - Remove pdflatex package

--- config/.config/nixpkgs/home.nix
+++ config/.config/nixpkgs/home.nix
@@ -3,26 +3,17 @@ let
   machine = import ./machine.nix;
   isLinux = machine.operatingSystem == "Linux";
   isDarwin = machine.operatingSystem == "Darwin";
-  tex = (pkgs.texlive.combine {
-    inherit (pkgs.texlive) scheme-basic
-      # Just the necessary packages to compile my Resume
-      marvosym
-      metafont
-      preprint
-      enumitem
-      fancyhdr
-      titlesec;
-  });
+  username = "chrlz";
   directory = (
-    if isLinux then "/home/chrlz"
-    else if isDarwin then "/Users/chrlz"
+    if isLinux then "/home/${username}"
+    else if isDarwin then "/Users/${username}"
     else throw "Unknown operating system"
   );
 in
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "chrlz";
+  home.username = username;
   home.homeDirectory = directory;
 
   # This value determines the Home Manager release that your
@@ -38,16 +29,7 @@ in
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  nixpkgs.overlays = [
-    (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
-    }))
-  ];
-
-
-
   home.packages = with pkgs; [
-    tex     # resume compilation
     gnupg   # Git GPG signing
     stow    # creating symlinks to .config and .local
     fzf     # tmux-sessionizer
@@ -68,51 +50,22 @@ in
     python
   ];
 
-  programs.exa = {
-    enable = true;
-  };
-
-  programs.gh = {
-    enable = true;
-    settings = {
-      editor = "nvim";
-    };
-  };
-
-  programs.bat = {
-    enable = true;
-    config = {
-      italic-text = "always";
-    };
-  };
-
+  programs.exa.enable = true;
+  programs.gh = { enable = true; settings = { editor = "nvim"; }; };
+  programs.bat = { enable = true; config = { italic-text = "always"; }; };
   programs.git = {
     enable = true;
     userName = "Charles Ancheta";
     userEmail = "55412395+cbebe@users.noreply.github.com";
     extraConfig = {
-      core = {
-        editor = "nvim";
-      };
-      color = {
-        ui = true;
-      };
-      init = {
-        defaultBranch = "master";
-      };
-      pull = {
-        ff = "only";
-      };
-      commit = {
-        gpgSign = true;
-      };
+      core = { editor = "nvim"; };
+      color = { ui = true; };
+      init = { defaultBranch = "master"; };
+      pull = { ff = "only"; };
+      commit = { gpgSign = true; };
     };
-    delta = {
-      enable = true;
-      options = {
-        navigate = true;
-        line-numbers = true;
-      };
+    delta = { enable = true;
+      options = { navigate = true; line-numbers = true; };
     };
   };
 }
