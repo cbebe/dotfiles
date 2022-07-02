4f3e8027a85234d57b5d5a32caf9451ac5db1219
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-07-02 07:29:54 -0600

    Remove deepcopy

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -86,29 +86,7 @@ function removeTrailingWhiteSpace()
   ]])
 end
 
--- Recursively copy a table by value (Deep copy)
-local function deepcopy(orig)
-    local orig_type = type(orig)
-    local copy
-    if orig_type == 'table' then
-        copy = {}
-        for orig_key, orig_value in next, orig, nil do
-            copy[deepcopy(orig_key)] = deepcopy(orig_value)
-        end
-        setmetatable(copy, deepcopy(getmetatable(orig)))
-    else -- number, string, boolean, etc
-        copy = orig
-    end
-    return copy
-end
-
-
-local function desc(str)
-  local data = deepcopy(opts)
-  data.desc = str
-  return data
-end
-
+local function desc(str) return { desc = str, unpack(opts) } end
 
 set('n', '<leader>cd', '<cmd>cd %:p:h<cr>',
   desc('change current directory to the file in the buffer'))
