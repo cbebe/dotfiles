9d61a126f4d073a9428fd5029653b5c24d37e1dc
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-07-04 22:08:11 -0600

    Add git gutter + go ham on formatting

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -32,6 +32,7 @@ SetTab(2, true)
 local function grp(name)
 	vim.api.nvim_create_augroup(name, { clear = true })
 end
+
 local au = vim.api.nvim_create_autocmd
 
 -- Render extra whitespace
@@ -40,29 +41,14 @@ vim.cmd([[
   match ExtraWhitespace /\s\+$/
 ]])
 local w = grp("RenderWhiteSpace")
-au("BufWinEnter", {
-	command = [[match ExtraWhitespace /\s\+$/]],
-	group = w,
-})
-au("InsertEnter", {
-	command = [[match ExtraWhitespace /\s\+\%#\@<!$/]],
-	group = w,
-})
-au("InsertLeave", {
-	command = [[match ExtraWhitespace /\s\+$/]],
-	group = w,
-})
-au("BufWinLeave", {
-	command = [[call clearmatches()]],
-	group = w,
-})
+au("BufWinEnter", { command = [[match ExtraWhitespace /\s\+$/]], group = w })
+au("InsertEnter", { command = [[match ExtraWhitespace /\s\+\%#\@<!$/]], group = w })
+au("InsertLeave", { command = [[match ExtraWhitespace /\s\+$/]], group = w })
+au("BufWinLeave", { command = [[call clearmatches()]], group = w })
 
 -- Highlight on Yank
 local yankGrp = grp("YankHighlight")
-au("TextYankPost", {
-	command = "lua require'vim.highlight'.on_yank({timeout = 40})",
-	group = yankGrp,
-})
+au("TextYankPost", { command = "lua require'vim.highlight'.on_yank({timeout = 40})", group = yankGrp })
 
 -- Edit with suda
 g.suda_smart_edit = 1
@@ -71,11 +57,7 @@ g.neoformat_try_node_exe = 1
 
 -- Auto-format JS/TS files
 local tsFormatGrp = grp("TSFormat")
-au("BufWritePre", {
-	command = "Neoformat prettier",
-	pattern = "*.{ts,tsx,js,jsx}",
-	group = tsFormatGrp,
-})
+au("BufWritePre", { command = "Neoformat prettier", pattern = "*.{ts,tsx,js,jsx}", group = tsFormatGrp })
 
 local opts = { noremap = true, silent = true }
 
@@ -93,71 +75,68 @@ local function desc(str)
 	return { desc = str, unpack(opts) }
 end
 
-set("n", "<leader>cd", "<cmd>cd %:p:h<cr>", desc("change current directory to the file in the buffer"))
+local function lset(key, fn, mapOpts)
+	set("n", "<leader>" .. key, fn, mapOpts)
+end
 
-set("n", "<leader>rs", removeTrailingWhiteSpace, desc("Remove all trailing whitespace"))
+lset("cd", "<cmd>cd %:p:h<cr>", desc("change current directory to the file in the buffer"))
+lset("rs", removeTrailingWhiteSpace, desc("Remove all trailing whitespace"))
 
-local function bindOpenFile(bind, file)
-	set("n", "<leader>" .. bind, "<cmd>e " .. file .. "<cr>", opts)
+lset("ff", "<cmd>Neoformat<cr>", desc("Format current file"))
+
+local function bindf(bind, file)
+	lset(bind, "<cmd>e " .. file .. "<cr>", opts)
 end
 
 -- Keybinds for commonly edited config files
-bindOpenFile("ve", "$MYVIMRC")
-bindOpenFile("vp", "$XDG_CONFIG_HOME/nvim/lua/plugins.lua")
-bindOpenFile("vl", "$XDG_CONFIG_HOME/nvim/lua/setup.lua")
-bindOpenFile("vn", "+40 $XDG_CONFIG_HOME/nixpkgs/home.nix")
+bindf("ve", "$MYVIMRC")
+bindf("vp", "$XDG_CONFIG_HOME/nvim/lua/plugins.lua")
+bindf("vl", "$XDG_CONFIG_HOME/nvim/lua/setup.lua")
+bindf("vn", "+40 $XDG_CONFIG_HOME/nixpkgs/home.nix")
 
 -- Hard time to break bad vim habits
-set("n", "<leader>h", "<cmd>HardTimeToggle<cr>", opts)
-
+lset("h", "<cmd>HardTimeToggle<cr>", opts)
 -- Reload config
-set("n", "<leader>vs", "<cmd>source $MYVIMRC<cr>", opts)
-
-set("n", "<leader>z", "<cmd>bd<cr>", desc("Close current buffer"))
-
-set("n", "<leader>o", "%O", desc("Insert after the last line in the enclosing brackets"))
+lset("vs", "<cmd>source $MYVIMRC<cr>", opts)
+lset("z", "<cmd>bd<cr>", desc("Close current buffer"))
+lset("o", "%O", desc("Insert after the last line in the enclosing brackets"))
 
 local function liveGrep(noHidden)
-	local args = { "rg", "--color=never", "--no-heading", "--with-filename", "--line-number", "--column" }
-	if noHidden then
-		table.insert(args, "--ignore-case")
-	else
-		table.insert(args, "--smart-case")
-		table.insert(args, "-u")
+	return function()
+		local args = { "rg", "--color=never", "--no-heading", "--with-filename", "--line-number", "--column" }
+		if noHidden then
+			table.insert(args, "--ignore-case")
+		else
+			table.insert(args, "--smart-case")
+			table.insert(args, "-u")
+		end
+		require("telescope.builtin").live_grep({ vimgrep_arguments = args })
 	end
-	require("telescope.builtin").live_grep({ vimgrep_arguments = args })
 end
 
 local function findFiles()
-	require("telescope.builtin").find_files({
-		find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
-	})
+	require("telescope.builtin").find_files({ find_command = { "rg", "--files", "--hidden", "-g", "!.git" } })
 end
 
-set("n", "<leader>l", findFiles, desc("Find files with ripgrep"))
-set("n", "<leader>F", findFiles, desc("Find files with ripgrep"))
-set("n", "<leader>fg", function()
-	liveGrep(true)
-end, desc("Live grep"))
-set("n", "<leader>fG", function()
-	liveGrep(false)
-end, desc("Live grep but also look for hidden files"))
-set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", opts)
-set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", opts)
-
-set("n", "<leader>.", vim.lsp.buf.code_action, desc("LSP Code action"))
-set("n", "<leader>bd", [[<cmd>%bd<bar>e#<bar>bd#<cr><bar>'"]], desc("Close all buffers except current one"))
+lset("l", findFiles, desc("Find files with ripgrep"))
+lset("F", findFiles, desc("Find files with ripgrep"))
+lset("fg", liveGrep(true), desc("Live grep"))
+lset("fG", liveGrep(false), desc("Live grep but also look for hidden files"))
+lset("fb", "<cmd>Telescope buffers<cr>", opts)
+lset("fh", "<cmd>Telescope help_tags<cr>", opts)
+lset(".", vim.lsp.buf.code_action, desc("LSP Code action"))
+lset("bd", [[<cmd>%bd<bar>e#<bar>bd#<cr><bar>'"]], desc("Close all buffers except current one"))
 
 set("n", "Y", "yg$", desc("Yank analogue of D"))
-set("n", "<leader>x", "<cmd>silent !chmod +x %<cr>", desc("Make current file executable"))
-set("n", "<leader>X", "<cmd>!./%<cr>", desc("Execute current file"))
+lset("x", "<cmd>silent !chmod +x %<cr>", desc("Make current file executable"))
+lset("X", "<cmd>!./%<cr>", desc("Execute current file"))
 
 set({ "n", "v" }, "<leader>y", '"+y', desc("Yank to clipboard register"))
-set("n", "<leader>Y", '"+Y')
+lset("Y", '"+Y')
 
-set("n", "<leader>cc", 'gg"+yG', desc("Yank the entire buffer into the clipboard"))
-set("n", "<leader>p", '"+p', desc("Paste clipboard contents"))
-set("n", "<leader>cv", 'ggVG"+p', desc("Replace the buffer with clipboard contents"))
+lset("c", 'gg"+yG', desc("Yank the entire buffer into the clipboard"))
+lset("p", '"+p', desc("Paste clipboard contents"))
+lset("cv", 'ggVG"+p', desc("Replace the buffer with clipboard contents"))
 
 if vim.fn.has("autocmd") then
 	vim.cmd("filetype plugin indent on")
@@ -170,20 +149,12 @@ set("n", "N", "Nzz", opts)
 
 set("x", "<leader>p", '"_dP', desc("The greatest remap ever"))
 
-set("n", "<leader>e", function()
-	require("harpoon.ui").toggle_quick_menu()
-end, desc("Open Harpoon quick menu"))
-
-set("n", "<leader>tc", function()
-	require("harpoon.cmd-ui").toggle_quick_menu()
-end, desc("Open Harpoon cmd quick menu"))
-
-set("n", "<leader>a", function()
-	require("harpoon.mark").add_file()
-end, desc("Mark file in harpoon"))
+lset("e", require("harpoon.ui").toggle_quick_menu, desc("Open Harpoon quick menu"))
+lset("tc", require("harpoon.cmd-ui").toggle_quick_menu, desc("Open Harpoon cmd quick menu"))
+lset("a", require("harpoon.mark").add_file, desc("Mark file in harpoon"))
 
 local navKeys = { "h", "j", "k", "l" }
-for i = 1, table.getn(navKeys) do
+for i = 1, #navKeys do
 	set("n", "<C-" .. navKeys[i] .. ">", function()
 		require("harpoon.ui").nav_file(i)
 	end, opts)
@@ -194,29 +165,23 @@ au("BufWritePre", {
 	command = "silent! lua require('go.format').gofmt()",
 })
 
-function mapLoveRun()
+function MapLoveRun()
 	set("n", "<localleader>r", function()
 		require("harpoon.tmux").sendCommand(1, "love .\n")
 	end, desc("Run love in current directory"))
 end
-local loveGrp = grp("Love2DDev")
-au("BufWinEnter", {
-	command = "lua mapLoveRun()",
-	pattern = "*.lua",
-	group = loveGrp,
-})
 
+local fmtGrp = grp("fmt")
+au("BufWritePre", { command = "undojoin | Neoformat", pattern = "*", group = fmtGrp })
+
+local loveGrp = grp("Love2DDev")
+au("BufWinEnter", { command = "silent! lua MapLoveRun()", pattern = "main.lua", group = loveGrp })
 au("BufWinEnter", { command = "HardTimeOn" })
 
--- au("FileType", {
---  pattern = "markdown",
---  command = "set foldexpr=NestedMarkdownFolds()"
--- })
-
-set("n", "<leader>s", function()
+lset("s", function()
 	require("harpoon.tmux").gotoTerminal(1)
 end, desc("Go to terminal 1 inside tmux"))
-set("n", "<leader>G", function()
+lset("G", function()
 	require("harpoon.tmux").sendCommand(1, "lazygit\n")
 end, desc("Open lazygit inside tmux"))
 
