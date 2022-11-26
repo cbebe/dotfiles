72658f38931989d4fd3da46fb540c15835cbe144
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-11-26 09:32:59 -0700

    Update to nightly

--- config/.config/nvim/lua/user/init.lua
+++ config/.config/nvim/lua/user/init.lua
@@ -1,4 +1,12 @@
 local config = {
+    updater = {
+      -- get nightly updates
+      channel = "nightly",
+      -- disable automatically reloading AstroNvim after an update
+      auto_reload = false,
+      -- disable automatically quitting AstroNvim after an update
+      auto_quit = false,
+    },
     diagnostics = {virtual_text = true, underline = true},
     header = {
         "████████ ██   ██ ██    ██  █████  ██   ██",
@@ -16,18 +24,37 @@ local config = {
     plugins = {
         init = {
             {"sbdchd/neoformat"}, {"nkrkv/nvim-treesitter-rescript"},
-            {"nvim-treesitter/nvim-treesitter-angular"}, {"LhKipp/nvim-nu"}
+            {"nvim-treesitter/nvim-treesitter-angular"}, {"LhKipp/nvim-nu"},
+            {
+                "lewis6991/hover.nvim",
+                config = function()
+                    require("hover").setup {
+                        init = function()
+                            -- Require providers
+                            require("hover.providers.lsp")
+                            -- require('hover.providers.gh')
+                            -- require('hover.providers.jira')
+                            -- require('hover.providers.man')
+                            -- require('hover.providers.dictionary')
+                        end,
+                        preview_opts = {
+                            border = nil
+                        },
+                        -- Whether the contents of a currently open hover window should be moved
+                        -- to a :h preview-window when pressing the hover keymap.
+                        preview_window = false,
+                        title = true
+                    }
+
+                    -- Setup keymaps
+                    vim.keymap.set("n", "K", require("hover").hover, {desc = "hover.nvim"})
+                    vim.keymap.set("n", "gK", require("hover").hover_select, {desc = "hover.nvim (select)"})
+                end
+            }
         }
     },
     polish = function()
         -- Set autocommands
-        vim.api.nvim_create_augroup("neofmt", {})
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            desc = "Format file with Neoformat on save",
-            group = "neofmt",
-            pattern = "*",
-            command = "silent Neoformat"
-        })
         vim.api.nvim_create_augroup("ShowDiagnostics", {})
         vim.api.nvim_create_autocmd("CursorHold,CursorHoldI", {
             desc = "Show line diagnostics automatically in hover window",
